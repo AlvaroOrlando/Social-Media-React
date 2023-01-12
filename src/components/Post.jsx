c90510128066f97bc5e42c4a727d7d209ad319cf
alvaro <alvarocondeorlando@gmail.com>
2023-01-12 02:13:25 -0300

    refatoração newComment State

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -8,11 +8,16 @@ import { Comment } from './Comment';
 import styles from './Post.module.css';
 
 
+// Post
 export function Post({ author, publishedAt, content }) {
+  
+// State Post
+const [comments, setComments] = useState([
+  'Post muito bacana, hein?'
+])
 
-  const [comments, setComments] = useState([
-    'Post muito bacana, hein?'
-  ])
+//State New Comment
+const [newCommentText, setNewCommentText] = useState('')
 
 const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
   locale:ptBR,
@@ -23,60 +28,68 @@ const publishedDateRelativeToNow =  formatDistanceToNow(publishedAt, {
   addSuffix: true,
 })
 
-
-
+// Create Comment FUNCTION
 function handleCreateNewComment(e){
   e.preventDefault();
-  const newCommentText = e.target.comment.value
   setComments([...comments, newCommentText ]);
-  e.target.comment.value = '';
-
+  setNewCommentText('');
 }
 
+// New Comment FUNCTION
+function handleNewCommentChange(e){
+  setNewCommentText(e.target.value)
+}
 
-    return (
-       <article className={styles.post}>
-        <header>
-            <div className={styles.author}>
-                <Avatar className={styles.avatar} src={ author.avatarUrl } alt="" />
-                <div className={styles.authorInfo}>
-                  <strong>{ author.name }</strong>
-                  <span>{ author.role }</span>
-                </div>
+// HTML
+return (
+    <article className={styles.post}>
+    <header>
+        <div className={styles.author}>
+            <Avatar className={styles.avatar} src={ author.avatarUrl } alt="" />
+            <div className={styles.authorInfo}>
+              <strong>{ author.name }</strong>
+              <span>{ author.role }</span>
             </div>
+        </div>
 
-            <time title= { publishedDateFormatted } dateTime= { publishedAt.toISOString() }>{ publishedDateRelativeToNow }</time>
-        </header>
+        <time title= { publishedDateFormatted } dateTime= { publishedAt.toISOString() }>{ publishedDateRelativeToNow }</time>
+    </header>
 
-        <div className={styles.content}>
-          {
-            content.map(line =>{
-             if(line.type === 'paragraph'){
-              return <p>{line.content}</p>
-             } else if(line.type ==='link'){
-              return <p><a href="">{line.content}</a></p>
-             }
-            })
+    <div className={styles.content}>
+      {
+        content.map(line =>{
+          if(line.type === 'paragraph'){
+          return <p>{line.content}</p>
+          } else if(line.type ==='link'){
+          return <p><a href="">{line.content}</a></p>
           }
-            {/* <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-            <p> Vitae recusandae maxime corrupti asperiores nesciunt, perspiciatis officia sapiente? Nostrum obcaecati </p>
-            <p><a href="www.clubedoscompositores.com.br">www.clubedoscompositores.com.br</a></p> */}
-        </div>
-        <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
-          <strong>Deixe seu feedback</strong>
-          <textarea 
-            name="comment"
-            placeholder='Deixe um comentário'
-          />
-          <footer>
-            <button type='submit'>Publicar</button>
-          </footer>
-        </form>
-        <div className={styles.commentList}>
-         {comments.map(comment =>{
-          return <Comment content={comment}/>
-         })}
-        </div>
-       </article> 
-    )
+        })
+      }
+        {/* <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+        <p> Vitae recusandae maxime corrupti asperiores nesciunt, perspiciatis officia sapiente? Nostrum obcaecati </p>
+        <p><a href="www.clubedoscompositores.com.br">www.clubedoscompositores.com.br</a></p> */}
+    </div>
+
+    {/* Textarea */}
+    <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
+      <strong>Deixe seu feedback</strong>
+      <textarea 
+        name="comment"
+        placeholder='Deixe um comentário'
+        onChange={handleNewCommentChange}
+        value={newCommentText}
+      />
+      <footer>
+        <button type='submit'>Publicar</button>
+      </footer>
+    </form>
+
+    {/* Comments */}
+    <div className={styles.commentList}>
+      {comments.map(comment =>{
+      return <Comment content={comment}/>
+      })}
+    </div>
+    </article> 
+)
 }
